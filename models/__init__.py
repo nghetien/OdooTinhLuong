75e3083e2dc4ebf8bb95c4e44169c3e8e79095a1
Tiến Nghê <a33062@thanglong.edu.vn>
2021-04-05 02:39:02 +0700

    02:38 Ban gan cuoi - Tien commit

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,3 @@
-from . import department, major, teacher, coefficient_teacher, coefficient_subject, subject, semester, course
+from . import department, major, teacher, subject, course
+from . import coefficient_teacher, coefficient_subject, semester
+from . import payroll_teacher
--- models/course.py
+++ models/course.py
@@ -3,7 +3,33 @@ from odoo import api, fields, models
 from datetime import date, datetime, time
 from datetime import timedelta
 from openerp.tools.translate import _
+import json
 
+class CostOfCourseForTeacher(models.Model):
+    _name = 'cost.of.course.for.teacher'
+    _description = "Cost Of Course For Teacher"
+    _rec_name = "id_cocft"
+
+    id_cocft = fields.Char(string='Mã', readonly=True)
+    semester = fields.Many2one('semester', string="Kỳ học", readonly=True)
+    teacher = fields.Many2one('teacher', string="Giảng viên", readonly=True)
+    coefficient_teacher = fields.Float(string='Hệ số học hàm', related='teacher.coefficient_teacher', readonly=True)
+    subject = fields.Many2one('subject', string="Môn học", readonly=True)
+    coefficient_subject = fields.Float(string='Hệ số môn học', related='subject.coefficient_subject', readonly=True)
+    credit = fields.Integer(string='Số tín chỉ', related='subject.credit', readonly=True)
+    teaching_time = fields.Integer(string="Số tiết", readonly=True)
+    number_of_students = fields.Integer(string="Số học sinh", readonly=True)
+    original_payroll = fields.Float(string="Lương cơ bản",readonly=True, default=200000)
+
+    coefficient_students = fields.Float(string="Hệ số học sinh", readonly=True)
+    sum_coefficient = fields.Float(string="Tổng hệ số", readonly=True)
+    sum_payroll = fields.Float(string="Tổng tiền lương", readonly=True)
+
+    @api.model
+    def create(self, vals):
+        vals['id_cocft'] = self.env['ir.sequence'].next_by_code('cost.course.sequence')
+        result = super(CostOfCourseForTeacher, self).create(vals)
+        return result
 
 class Course(models.Model):
     _name = "course"
@@ -14,22 +40,47 @@ class Course(models.Model):
     name_course = fields.Char(string='Tên lớp học', required=True)
     semester = fields.Many2one('semester', string="Kỳ học")
 
-    id_teacher = fields.Many2one('teacher', string="Mã giáo viên giảng dạy", required=True)
-    name_teacher = fields.Char(string='Tên giáo viên giảng dạy', related='id_teacher.name_teacher', readonly=True)
-    department_of_teacher = fields.Many2one('department', string='Khoa chủ quản giáo viên', related='id_teacher.name_department', readonly=True)
-    rank_teacher = fields.Many2one('coefficient.teacher', string='Học hàm của giáo viên', related='id_teacher.rank', readonly=True)
-    coefficient_teacher = fields.Float(string='Hệ số học hàm', related='id_teacher.coefficient_teacher', readonly=True)
+    teacher = fields.Many2one('teacher', string="Giảng viên", required=True)
+    department_of_teacher = fields.Many2one('department', string='Khoa', related='teacher.name_department', readonly=True)
+    rank_teacher = fields.Many2one('coefficient.teacher', string='Học hàm giáo viên', related='teacher.rank', readonly=True)
+    coefficient_teacher = fields.Float(string='Hệ số học hàm', related='teacher.coefficient_teacher', readonly=True)
 
-    id_subject = fields.Many2one('subject', string="Mã môn học", required=True)
-    name_subject = fields.Char(string='Tên môn học', related='id_subject.name_subject', readonly=True)
-    rank_subject = fields.Many2one('coefficient.subject', string='Độ khó', related='id_subject.rank', readonly=True)
-    coefficient_subject = fields.Float(string='Hệ số môn học', related='id_subject.coefficient_subject', readonly=True)
-    course_sequence = fields.Char(string='Mã lớp học tăng', required=True, copy=False,
-                                        readonly=True, index=True, default=lambda self: _('New'))
+    subject = fields.Many2one('subject', string="Môn học", required=True)
+    credit = fields.Integer(string='Số tín chỉ', related='subject.credit', readonly=True)
+    rank_subject = fields.Many2one('coefficient.subject', string='Độ khó', related='subject.rank', readonly=True)
+    coefficient_subject = fields.Float(string='Hệ số môn học', related='subject.coefficient_subject', readonly=True)
+
+    teaching_time = fields.Integer(string="Số tiết", required=True)
+    number_of_students = fields.Integer(string="Số học sinh", required=True)
 
     @api.model
     def create(self, vals):
-        vals['course_sequence'] = self.env['ir.sequence'].next_by_code('course.sequence')
-        vals['id_course'] = str(vals['name_course']) + vals['course_sequence']
+        vals['id_course'] = self.env['ir.sequence'].next_by_code('course.sequence')
+        vals['id_course'] = str(vals['name_course']) + vals['id_course']
         result = super(Course, self).create(vals)
+
+        coefficient_students = 0
+        if result.number_of_students < 20:
+            coefficient_students = -0.5
+        elif (result.number_of_students >= 20 and result.number_of_students <= 40):
+            coefficient_students = 0
+        else:
+            coefficient_students = 0.2
+        sum_coefficient = result.teaching_time * (coefficient_students + result.coefficient_subject + result.coefficient_teacher)
+        sum_payroll = 200000 * sum_coefficient
+
+        data_cost = {
+            'id_cocft': result.id_course,
+            'semester': vals['semester'],
+            'teacher': vals['teacher'],
+            'subject': vals['subject'],
+            'teaching_time': vals['teaching_time'],
+            'number_of_students': vals['number_of_students'],
+            'coefficient_students': coefficient_students,
+            'sum_coefficient': sum_coefficient,
+            'sum_payroll': sum_payroll
+        }
+        create_new_cost_course = super(CostOfCourseForTeacher, self.env['cost.of.course.for.teacher']).create(data_cost)
         return result
+
+    
